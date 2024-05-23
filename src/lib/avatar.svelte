c12ee53451605ad5b2950b5f2a5bf58fc24ca644
john <john8.roosevelt@gmail.com>
2024-05-23 19:53:22 +0800

    add avatar

--- src/lib/avatar.svelte
+++ src/lib/avatar.svelte
@@ -1,7 +1,19 @@
 <script>
-  const { data, item } = $props();
+  import { Identicon } from "svelte-identicons";
+
+  const { data, item, size = 9 } = $props();
 </script>
 
-<div flex-bc>
-  <span i-carbon-user-avatar text-4xl > </span>
+<div flex-bc rounded-2 overflow-hidden>
+  <!-- <span i-carbon-user-avatar text-4xl> </span> -->
+  <Identicon
+    seed="your-seed"
+    height={size}
+    width={size}
+    pixelSize={4}
+    numberOfColors={3}
+    symetry="central"
+    text={undefined}
+    textColor="#ffffff"
+  />
 </div>
--- src/routes/(authed)/(home)/my/+page.svelte
+++ src/routes/(authed)/(home)/my/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Avatar from "$lib/avatar.svelte";
+
 	const { data } = $props();
 	let { user } = data;
 </script>
@@ -6,7 +8,7 @@
 <article h-full bg-gray-100 space-y-4>
 	<a href="#/" w-full bg-white flex flex-col px-6 pt-16 pb-6>
 		<div flex class="justify-between items-center" space-x-4>
-			<span i-carbon-user-avatar text-6xl></span>
+			<Avatar size=12 />
 			<div flex flex-col flex-1>
 				<div text-3xl>{user.name}</div>
 				<div text-gray>ID: {user.id}</div>
