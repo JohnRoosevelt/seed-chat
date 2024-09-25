da7996349bbd497f6ef9150093d1e46e497b9787
john <john8.roosevelt@gmail.com>
2024-09-25 18:35:16 +0800

    try confirm email

--- src/routes/register/confirm/email/+page.svelte
+++ src/routes/register/confirm/email/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import {  goto  } from "$app/navigation";
+  import { goto } from "$app/navigation";
   import { confirmRegisterEmail } from "$lib";
   // /register/confirm/email?token=1a1ecdba2cb0805a6b7c14e19609629e9f38c5cec5f62fdbb8fa492e9afc995c020dac96f98caf418a569518525e58d73fc8acbe602f6ccaa725c5bd7f439fb0&tokenId=66f11c9e6f36d570da93f8c2
   let token = $state();
@@ -22,12 +22,13 @@
     });
     console.log({ statusCode, errorCode, error });
     isSuccess = statusCode === 200;
-    errorInfo = `${errorCode}, ${error}! 5秒后自动返回注册界面`;
-    if (!isSuccess) {
-      setTimeout(() => {
-        goto('/register')
-      }, 5000);
-    }
+    errorInfo = `${errorCode}, ${error}! 返回注册（10 秒后自动跳转）`;
+    const redirectUrl = isSuccess ? "/login" : "/register";
+
+    setTimeout(() => {
+      goto(redirectUrl);
+    }, 10000);
+
     loading = false;
   });
 </script>
@@ -36,7 +37,9 @@
   {#if loading}
     <span>loading</span>
   {:else if isSuccess}
-    <a href="/login" text-sm text-primary-600> 邮箱确认成功，去登录 </a>
+    <a href="/login" text-3 text-primary-600>
+      邮箱确认成功，去登录 （10 秒后自动跳转）
+    </a>
   {:else}
     <a href="/register" text-3 text-red-600 p-5>{errorInfo}</a>
   {/if}
