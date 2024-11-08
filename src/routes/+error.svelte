ed508ed7fdf2ce2168f6945610b08d718b12654d
john <john8.roosevelt@gmail.com>
2024-11-08 10:23:40 +0800

    add error

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,3 +1,15 @@
-<article w-screen h-screen flex-cc border>
-  you are on the moon.
-</article>
+<script>
+  import Main from "$com/main.svelte";
+  import { page } from "$app/stores";
+</script>
+
+<Main>
+  <article w-screen h-screen flex-cc flex-col space-y-8>
+    <p> {$page.error.message} </p>
+
+    <a href="/" data-sveltekit-replacestate flex-cc flex-col text-blue>
+      <span i-carbon-home text-4xl> </span>
+      <span text-xs uppercase> 首页 </span>
+    </a>
+  </article>
+</Main>
