209279c02dfc5297ba599eb6c6d0f172812225b7
john <john8.roosevelt@gmail.com>
2024-11-10 08:11:01 +0800

    home grid

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -5,10 +5,10 @@
 </script>
 
 <Main>
-  <ul w-full px-5 space-y-4>
+  <ul w-full px-5 grid grid-cols-2 gap-4 justify-center items-center md="grid-cols-3" lg="grid-cols-4">
     {#each data.c as item}
-      <li>
-        <a href="/home/{item.id}/" data-sveltekit-replacestate> {item.name} </a>
+      <li bg-gray200 rounded-2 w-full h-20 flex-cc hover="bg-gray-300">
+        <a w-full h-full flex-cc href="/home/{item.id}/" data-sveltekit-replacestate> {item.name} </a>
       </li>
     {/each}
   </ul>
