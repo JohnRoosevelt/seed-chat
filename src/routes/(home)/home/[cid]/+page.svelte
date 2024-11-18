1a395fc9b514d2c2a19cb310867c181021501367
john <john8.roosevelt@gmail.com>
2024-11-18 11:09:10 +0800

    add audio

--- src/routes/(home)/home/[cid]/+page.svelte
+++ src/routes/(home)/home/[cid]/+page.svelte
@@ -1,7 +1,11 @@
 <script>
   import Back from "$com/back.svelte";
+  import AudioPlayer from "$com/AudioPlayer.svelte";
 
   const { data } = $props();
+  let curPlayIndex = $state(0)
+  let src = $derived(`https://pub-42fa17a0d06841f295980294e061513c.r2.dev/home/${data.cItem.name}/${data.cItem.articles[curPlayIndex].name}.mp3`)
+  let artist = $derived(data.cItem.articles[curPlayIndex].name)
 </script>
 
 <svelte:head>
@@ -18,11 +22,16 @@
     <div></div>
   {/snippet}
 
-  <ul w-full px-5 space-y-2>
-    {#each data.cItem.articles as item}
-      <li>
-        <a href="/home/{data.cItem.id}/{item.id}/">{item.id} {item.name} </a>
-      </li>
+  <ul w-full px-5 pb-15 space-y-2>
+    {#each data.cItem.articles as item, i}
+    <li>
+      <!-- <a href="/home/{data.cItem.id}/{item.id}/">{item.id} {item.name} </a> -->
+      <button class:text-green={i == curPlayIndex} onclick={() => curPlayIndex = i}>{item.id} {item.name} </button>
+    </li>
     {/each}
   </ul>
+
+  <footer fixed z-9 bottom-0 w-full>
+    <AudioPlayer {src} title={data.cItem?.name} {artist} onended={() => curPlayIndex == data.cItem.articles.length - 1 ? curPlayIndex = 0 : curPlayIndex ++} />
+  </footer>
 </Back>
